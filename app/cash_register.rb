8f71dc0e0d1c9dd5961df072583924d29a70aae0
Jan Matuszewski <matuszewski.jan@hotmail.com>
2022-11-21 20:32:50 +0100

    Improve Cash Register logic

--- app/cash_register.rb
+++ app/cash_register.rb
@@ -2,15 +2,14 @@ require './app/cash_register_cli'
 require './app/cart'
 
 puts 'Hello to Cash Register!'
-puts '`help` will show you all available commands'
-puts 'Currently available products are:'
 
 cart = Cart.new
-cli = CashRegisterCLI.new(cart:)
+cli = CashRegisterCLI.new(cart)
 
-puts CashRegisterCLI::PRODUCTS
 puts cli.help
+puts 'Currently available products are:'
+puts cli.list
 
 while true
-  cli.prompt('> ')
+  puts cli.prompt('> ')
 end
--- app/cash_register_cli.rb
+++ app/cash_register_cli.rb
@@ -1,13 +1,21 @@
+# frozen_string_literal: true
+
+require './app/products'
+require './app/price_calculator'
+
 class CashRegisterCLI
-  attr_accessor :basket
+  attr_accessor :cart, :price_calculator
 
   def initialize(cart)
     @cart = cart
+    @price_calculator = PriceCalculator.new(cart:)
   end
 
   def prompt(label)
     print(label)
     value = gets.chomp.split
+
+    return if value.length == 0
 
     if value.length == 1
       send(value[0].to_sym)
@@ -17,13 +25,32 @@ class CashRegisterCLI
   end
 
   def help
-    puts 'Available commands are:'
-    puts '`exit`'
-    puts '`add CODE|NAME AMOUNT`: amount could be skipped if only one added'
+    <<~HELP
+      Available commands are:
+      `help`: display all commands
+      `exit`
+      `list`: display list with all available products
+      `add CODE AMOUNT`: amount could be skipped if only one added, eg. `add strawberries 4`
+      `total`: see what is already added to cart
+    HELP
+  end
+
+  def list
+    Products::LIST.map { |p| "#{p[:name]} (#{p[:code]})"}
   end
 
   def add(name, amount = 1)
-    @cart.add_product(name, amount)
+    @cart.add_product(name, amount.to_i)
+    price_calculator.process
+    total
+  end
+
+  def total
+    products = @cart.products.map do |product|
+      "#{product[:name]} - #{product[:discounted_price] || product[:price]}"
+    end.join("\n")
+
+    "#{products}\n#{@cart.total_price}. Discount amount: #{@cart.total_discounts}."
   end
 
   def method_missing(name, *args, &block)
