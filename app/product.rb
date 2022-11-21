59c0b504ce4437e16db293353bda231183b52fde
Jan Matuszewski <matuszewski.jan@hotmail.com>
2022-11-21 20:56:32 +0100

    Add #format to Product

--- app/product.rb
+++ app/product.rb
@@ -2,12 +2,22 @@
 
 require 'money'
 
+Money.locale_backend = nil
+
 class Product
-  attr_accessor :code, :name, :price
+  attr_accessor :code, :name, :price, :discounted_price
 
   def initialize(code:, name:, price:)
     @code = code
     @name = name
-    @price = Money.from_cents(price, 'EUR')
+    @price = price
+  end
+
+  def format(without_price: false)
+    if without_price
+      "#{name} (#{code})"
+    else
+      "#{name} - #{Money.from_cents(discounted_price || price, 'EUR').format}"
+    end
   end
 end
---
+++ spec/product_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './app/product'
+
+RSpec.describe Product do
+  describe '#format' do
+    subject(:product) { described_class.new(code: 'tt', name: 'test', price: 1000) }
+
+    context 'when without_price is true' do
+      it 'return only name and code' do
+        expect(product.format(without_price: true)).to eq "test (tt)"
+      end
+    end
+
+    context 'when without_price is false' do
+      it 'return name and prices' do
+        expect(product.format).to eq "test - €10.00"
+      end
+    end
+  end
+end
