01733855cdff12951558e7b795447b7f3405ae47
Jan Matuszewski <matuszewski.jan@hotmail.com>
2022-11-22 12:15:07 +0100

    Add tests for PriceCalculator

--- app/price_calculator.rb
+++ app/price_calculator.rb
@@ -13,11 +13,22 @@ class PriceCalculator
     Promotions::LIST.each do |promotion|
       promotion.calculate_discounts!(cart.products)
     end
-    cart.total_price = cart.products.sum { |p| p.discounted_price || p.price }
-    cart.total_discounts = cart.products.select { |p| p.discounted_price }.sum { |p| p.price - p.discounted_price }
+
+    cart.total_price = calculate_total_price
+    cart.total_discounts = calculate_total_discounts
   end
+
+  private
 
   def reset_discounts
     cart.products.each { |p| p.discounted_price = nil }
+  end
+
+  def calculate_total_price
+    cart.products.sum { |p| p.discounted_price || p.price }
+  end
+
+  def calculate_total_discounts
+    cart.products.select { |p| p.discounted_price }.sum { |p| p.price - p.discounted_price }
   end
 end
---
+++ spec/price_calculator_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './app/price_calculator'
+require './app/cart'
+
+RSpec.describe PriceCalculator do
+  def prepare_cart
+    cart = Cart.new
+    cart.add_product('GR1', 6)
+    cart.add_product('SR1', 3)
+    cart.add_product('CF1', 1)
+    cart
+  end
+
+  describe '#process' do
+    subject(:calculator) { described_class.new(cart: prepare_cart) }
+
+    let(:cart) { calculator.cart }
+
+    context 'when some products removed from cart after calculation' do
+      before { calculator.process }
+
+      it 'calculate proper promotions' do
+        cart.remove_product('gr1', 3)
+        calculator.process
+        expect(cart.products.select { |p| p.code == 'GR1' && p.discounted_price != nil }.length).to eq 1
+      end
+    end
+
+    it 'calculate promotions for green tea' do
+      calculator.process
+
+      expect(cart.products.select { |p| p.code == 'GR1' && p.discounted_price != nil }.length).to eq 3
+    end
+
+    it 'calculate promotions for strawberries' do
+      calculator.process
+      strawberries = cart.products.select { |p| p.code == 'SR1' }
+
+      expect(strawberries.length).to be > 0
+      expect(strawberries.all? { |s| s.discounted_price == 4.5 }).to eq true
+    end
+
+    it 'does not calculate promotions for coffee' do
+      calculator.process
+      coffee = cart.products.select { |p| p.code == 'CF1' }
+
+      expect(coffee.length).to be > 0
+      expect(coffee.all? { |s| s.discounted_price == nil }).to eq true
+    end
+  end
+end
