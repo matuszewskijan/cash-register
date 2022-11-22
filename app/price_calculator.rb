2bc44d9b6d5d12f3896bd1d19ee6a4eb387c0907
Jan Matuszewski <matuszewski.jan@hotmail.com>
2022-11-22 12:06:26 +0100

    Reset all discounts before processing PriceCalculator

--- app/price_calculator.rb
+++ app/price_calculator.rb
@@ -8,10 +8,16 @@ class PriceCalculator
   end
 
   def process
+    reset_discounts
+
     Promotions::LIST.each do |promotion|
       promotion.calculate_discounts!(cart.products)
     end
     cart.total_price = cart.products.sum { |p| p.discounted_price || p.price }
     cart.total_discounts = cart.products.select { |p| p.discounted_price }.sum { |p| p.price - p.discounted_price }
+  end
+
+  def reset_discounts
+    cart.products.each { |p| p.discounted_price = nil }
   end
 end
